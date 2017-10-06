503abc670164911049ef89a34876e15c98e5beb1
Abrazova Alesia <Obrazova_1998@mail.ru>
2017-10-06 11:42:52 +0300

    Reverse

--- app/src/main/java/alexandriaobraz/github/com/homework/MainActivity.java
+++ app/src/main/java/alexandriaobraz/github/com/homework/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //create conflict 2
+      //create conflict 2
+
     }
 }
